10da3956512e8c691999e8a46f2bf5c78d2317ff
fhirdosbasha <bsfhirdosbasha@gmail.com>
2023-11-30 12:16:03 +0530

    base changed to repository name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/day68_combine-reducers-in-redux-in-react/',
+  base: '/basic-transaction-display-using-redux/',
   plugins: [react()],
 })
